d0b8170dbeb951ca50d4128b6df1e1e482da929a
Kevin Hur <kevinhur99@gmail.com>
2023-04-12 22:05:18 -0400

    Initial Setup and Movement
    - Investigated different options for set-up
    - Created movement for sample character
    
    Time: 2 hours

---
+++ Unity Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _movementInput * _speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();
+    }
+}
